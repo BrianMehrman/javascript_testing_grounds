56ee9f29ef68bde728c8caf5a47a1c83f6df28d2
Brian Mehrman <bcmehrman@gmail.com>
2012-06-13 22:45:36 -0500

    update again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,24 +15,23 @@ class PagesController < ApplicationController
   
   def download_images
     save_base = "public/system/images"
-    
     count = params[:count]
     @images = []
     count.to_i.times do |i|
       # curl images to local storage then load them
       img_path = "#{save_base}/test_#{i}.jpg"
       saveKitty img_path
-      @images << img_path
+      @images << img_path.gsub(/^public\//,'')
     end
   end
 
   def getKitty 
-    File.join("http://placekitten.com", (256 + rand(20)).to_s, (256 + rand(20)).to_s)
+    File.join("http://placekitten.com", (500 + rand(20)).to_s, (500 + rand(20)).to_s)
   end
   
   def saveKitty here
     require 'open-uri'
-    cat_url = File.join("http://placekitten.com", (256 + rand(20)).to_s, (256 + rand(20)).to_s)
+    cat_url = File.join("http://placekitten.com", (500 + rand(20)).to_s, (500 + rand(20)).to_s)
     newCat = open(here,'wb')
     newCat.write(open(cat_url).read)
     newCat.close
